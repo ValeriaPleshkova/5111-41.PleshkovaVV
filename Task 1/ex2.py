0be754656b56210efde72da899fcc62384fce23f
“Плешкова <plshkv.lerikka@gmail.com>
2022-09-28 19:34:42 +0300

    Проверка работы

---
+++ Task 1/ex2.py
@@ -0,0 +1,9 @@
+suvenir_weight = 75
+useles_weight = 112 
+
+suvenir_count = int (input("Введите число сувениров:"))
+useles_count = int(input("Введите число безделушек"))
+
+
+print(suvenir_weight,useles_weight,suvenir_count,useles_count)
+print(suvenir_weight*suvenir_count + useles_count*useles_weight)
--- Task 1/main.py
+++ Task 1/main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+name = input("Введите имя:" )
+surname = input("Введите фамилию:")
+
+country = input ("Введите страну:")
+city = input ("Введите город:")
+street = input ("Введите улицу:")
+house = input ("Введите дом:")
+appartment = input("Введите квартиру:")
+
+print(name, surname)
+print(country, city, street, house, appartment)
